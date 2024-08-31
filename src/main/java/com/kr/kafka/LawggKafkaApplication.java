b0afac52a5ecd5cd3f63ec951b635803e1a74ac7
beomshin.kim <chzh859@naver.com>
2024-08-31 23:05:43 +0900

    Initial commit

---
+++ src/main/java/com/kr/kafka/LawggKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kr.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawggKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawggKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kr/kafka/LawggKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawggKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
